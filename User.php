0b13ada8d896dd01f32e8507033683371104f84f
Juma Hakika <mohamedhakika@gmail.com>
2019-01-12 22:32:23 +0300

    Adding getEmail method on user.php

--- User.php
+++ User.php
@@ -2,8 +2,15 @@
     $first_name = "Juma";
     $last_name = "Hakika";
 
-    function fullname($first, $last){
+    function fullname($first, $last)
+    {
         return $first. " ".$last;
+    }
+
+    function getEmail($first, $last)
+    {
+        $email = $first.".".$last."@openclassrooms.com";
+        return strtolower($email);
     }
 
 ?>
